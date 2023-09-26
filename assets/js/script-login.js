578867fee4b9e8bdbb5cd93020c923aaed12b677
Amaral <dmrramaral@gmail.com>
2023-09-26 11:29:17 -0300

    Correção de direcionamento após login, retirei o form

--- assets/js/script-login.js
+++ assets/js/script-login.js
@@ -4,28 +4,35 @@ btn.addEventListener('click', function () {
     let email = document.querySelector('#email').value;
     let senha = document.querySelector('#password').value;
 
-    if (email == 'dmrramaral@gmail.com' && senha == '123') {
+    if (email == "dmrramaral@gmail.com" && senha == "123") {
 
         document.querySelector('.popUpLogin').insertAdjacentHTML(
             "afterbegin",
-            '<div class="confirmLogin">USUÁRIO LOGADO COM SUCESSO</div>'
-
+            `<div class="confirmLogin">
+            USUÁRIO LOGADO COM SUCESSO
+            </div>`
         )
-
+        setTimeout(function () {
+            document.querySelector('.popUpLogin').innerHTML = "";
+            window.location.href = "/paginas/home.html";
+        }, 3000); 
+        
     }
     else {
         document.querySelector('.popUpLogin').insertAdjacentHTML(
             "afterbegin",
-            '<div class="errorLogin"><span>USUÁRIO OU SENHA INCORRETOS</span> </div>'
+            `<div class="errorLogin"><span>USUÁRIO OU SENHA INCORRETOS</span> </div>`
         )
         email = document.querySelector('#email').value = '';
         senha = document.querySelector('#password').value = '';
-
-
-    }
-
-
-
+       
+        setTimeout(function () {
+            document.querySelector('.popUpLogin').innerHTML = "";
+            
+        }, 3000); 
+        
+    }      
+   
 }
 );
 
